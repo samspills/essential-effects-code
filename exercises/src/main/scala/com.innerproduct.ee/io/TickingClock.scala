2d0dd6db5312d68c905e764475ff4cac1461023c
Sam Pillsworth <srpillsworth@gmail.com>
2021-04-18 21:27:01 -0400

    tickingClock -- two implementations
    Thought Process
    - started with implementation -ForComp
    - while trying to figure out the bit about recursion I was looking at
      cats IOApp docs
    - saw the example with IO.suspend talking about cancellation which is
      what led to the applicative -Composition version
    
    Haven't looked at the solution branch yet, but I'm going to guess their
    solution looks more like the -ForComp version.
    
    Q: w-out IO.suspend, the -Composition method doesn't work. I guess makes
    sense (maybe??) b/c the applicative composition is equivalent to mapN,
    not a flatMap...
    But then what is IO.suspend doing? Is there a flatMap in there? I
    haven't gone to the source code yet I've just been trying to read things
    online, and it's not super clear yet.

--- exercises/src/main/scala/com.innerproduct.ee/io/TickingClock.scala
+++ exercises/src/main/scala/com.innerproduct.ee/io/TickingClock.scala
@@ -2,9 +2,22 @@ package com.innerproduct.ee.io
 
 import cats.effect._
 
+import scala.concurrent.duration._
+
 object TickingClock extends IOApp {
   def run(args: List[String]): IO[ExitCode] =
-    tickingClock.as(ExitCode.Success)
+    tickingClockForComp.as(ExitCode.Success)
 
-  val tickingClock: IO[Unit] = ??? // <1>
+  val tickingClockForComp: IO[Unit] = for {
+    _ <- IO(println(System.currentTimeMillis()))
+    _ <- IO.sleep(1.second)
+    tc <- tickingClockForComp
+  } yield tc
+
+  val tickingClockComposition: IO[Unit] =
+    IO.suspend(
+      IO(println(System.currentTimeMillis())) *> IO
+        .sleep(1.second) *> tickingClockComposition
+    )
+
 }
