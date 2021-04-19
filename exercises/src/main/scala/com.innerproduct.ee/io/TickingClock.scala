90437b3a8c877b3009609553a1d4053be1066ebe
Sam Pillsworth <srpillsworth@gmail.com>
2021-04-18 21:56:19 -0400

    guaranteeCase learnings
    (after looking at solution)
    
    - guaranteeCase executes some `finalizer` when the source is finished
    - basically lets us clean up and do whatever handling based on whether
      IO succeeded, cancelled, or errored
    - in terms of user experience, I'm running this in sbt and cancelling
      still dumps a big trace. The only difference is that the cancellation
      message is printed
    
    - solution also uses a debug method instead of just printing to console,
    this might make it easier to see what behaviour is changing with
    addition of guaranteeCase?? (haven't looked at debug yet)

--- exercises/src/main/scala/com.innerproduct.ee/io/TickingClock.scala
+++ exercises/src/main/scala/com.innerproduct.ee/io/TickingClock.scala
@@ -6,7 +6,16 @@ import scala.concurrent.duration._
 
 object TickingClock extends IOApp {
   def run(args: List[String]): IO[ExitCode] =
-    tickingClockForComp.as(ExitCode.Success)
+    tickingClockForComp
+      .guaranteeCase {
+        case ExitCase.Canceled =>
+          IO(println("TickingClock was cancelled beep boop"))
+        case ExitCase.Completed =>
+          IO(println("TickingClock completed (we should never see this?)"))
+        case ExitCase.Error(e) =>
+          IO(println(s"TickingClock failure with error: $e"))
+      }
+      .as(ExitCode.Success)
 
   val tickingClockForComp: IO[Unit] = for {
     _ <- IO(println(System.currentTimeMillis()))
